5b7ef1780d45e17a9f7820d6879265a28a1cb70a
t-ichino <tmkicn@gmail.com>
2016-09-29 16:01:37 +0900

    callback.php added

--- test.php
+++ test.php
@@ -2,11 +2,19 @@
 include("vendor/pubsubhubbub/subscriber/src/Subscriber.php");
 use \Pubsubhubbub\Subscriber\Subscriber;
 
-$hub_url = "http://pubsubhubbub.appspot.com";
-$callback_url = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+$callback_func = "callback.php";
+$hub_url = "https://pubsubhubbub.appspot.com";
+// $hub_url = "https://pubsubhubbub.superfeedr.com/";
+$callback_url = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $callback_func;
 
 $s = new Subscriber($hub_url, $callback_url);
 $feed = "http://feeds.feedburner.com/onlineaspect";
+// $feed = "http://rss.rssad.jp/rss/itmtop/2.0/itmedia_all.xml";
 
-$s->subscribe($feed);
-
+$res = $s->subscribe($feed);
+if($res){
+  var_dump($res);
+}else{
+  echo "subscribe failed";
+//  echo $res;
+}
